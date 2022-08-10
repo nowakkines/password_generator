08fb819ffcc71727368542c580afaf03ef734d8f
ap7kj <spearekurwa@yandex.com>
2022-08-10 18:22:53 +0300

    feat: new functions

---
+++ password.py
@@ -0,0 +1,19 @@
+from string import ascii_lowercase
+from string import ascii_uppercase
+from string import digits
+from string import punctuation
+from random import choice
+
+CHARS = ''
+
+def hello():
+    ''
+
+def setup():
+    pass
+
+def generate():
+    pass
+
+def result():
+    pass
