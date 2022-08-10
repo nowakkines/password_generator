8f3a090db78b771a790ebd0323f0fbf48420d791
ap7kj <spearekurwa@yandex.com>
2022-08-10 18:47:26 +0300

    feat: added conditions for customization

--- password.py
+++ password.py
@@ -10,7 +10,24 @@ def hello():
     ''
 
 def setup():
-    pass
+    global chars
+    match console.input('-- Would you like to include [blue]numbers[/blue]? (+) --\n'):
+        case '+':
+            chars += digits
+    match console.input('-- Would you like to include [blue]uppercase[/blue] letters? (+) --\n'):
+        case '+':
+            chars += ascii_lowercase
+    match console.input('-- Would you like to include [blue]lowercase[/blue] letters? (+) --\n'):
+        case '+':
+            chars += ascii_lowercase
+    match console.input('-- Would you like to include [blue]punctuation?[/blue] (+) --\n'):
+        case '+':
+            chars += punctuation
+    match console.input('-- Would you like to exclude [blue]ambiguous characters il1Lo0O[/blue] (+) --\n'):
+        case '+':
+            for symbols in 'il1Lo0O':
+                chars += chars.replace(symbols, '')
+
 
 def generate():
     pass
